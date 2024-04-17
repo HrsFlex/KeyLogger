ea1a2d4b32420c0a4a77953f245df35db8a49649
HrsFlex <harsh42026@gmail.com>
2024-04-17 08:50:35 +0530

    Simple FrontEnd Scripting

---
+++ index.js
@@ -0,0 +1,6 @@
+const logDiv = document.getElementById('log');
+const stateDiv = document.getElementById('state');
+const startDiv = document.getElementById('start-btn');
+const stopDiv = document.getElementById('stop-btn');
+
+
