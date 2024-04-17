93adb8bffd3c5e1534d15bc078dfaef6d7cbfabb
HrsFlex <harsh42026@gmail.com>
2024-04-17 09:10:40 +0530

    Testing and Debugging

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const logDiv = document.getElementById('log');
 const stateDiv = document.getElementById('state');
-const startDiv = document.getElementById('start-btn');
-const stopDiv = document.getElementById('stop-btn');
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
 
-
+startBtn.addEventListner("click",()=>{
+    document.addEventListner("keydown",handleDown);
+    document.addEventListner("keyup",handleUp);
+    startBtn.disabled = true;
+    stopBtn.disabled = true;
+})
+stopBtn.addEventListner("click",()=>{
+    document.removeEventListner("keydown",handleDown);
+    document.removeEventListner("keyup",handleUp);
+    logDiv.textContent = "";
+    stateDiv.textContent = "";
+    stopBtn.disabled = true;
+    startBtn.disabled = true;
+})
+function handleDown(e){
+    logDiv.textContent = `Key ${e.key} is pressed`;
+    stateDiv.textContent = "key is down";
+}
+function handleUp(e){
+    logDiv.textContent = `Key ${e.key} is pressed up`;
+    stateDiv.textContent = "key is up";
+}
